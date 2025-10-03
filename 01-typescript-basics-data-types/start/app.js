9bd59fbe172b5aa6aa98c953ed954d614677e537
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-04 01:33:02 +0300

    TS Variable

---
+++ 01-typescript-basics-data-types/start/app.js
@@ -0,0 +1,2 @@
+//Typescript Lesson
+console.log("Hello Typescript Please Be Good");
---
+++ 01-typescript-basics-data-types/start/app.ts
@@ -0,0 +1,6 @@
+/* Lecture 3 : Variables In Typescript */
+
+let num = 100;
+num = 20; //Reassigning
+
+const text = "Hello Typescript";
