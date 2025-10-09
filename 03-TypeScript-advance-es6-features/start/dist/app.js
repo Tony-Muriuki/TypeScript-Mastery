a365eb7fb3d9ea81ec990ec883c636b40a5e6a4c
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-09 20:07:43 +0300

    Array Destructuring

--- 03-TypeScript-advance-es6-features/start/dist/app.js
+++ 03-TypeScript-advance-es6-features/start/dist/app.js
@@ -1,6 +1,6 @@
+"use strict";
 // Array Destructuring
 const person = ["John", "Smith", 28];
-
 const [fname, lmane, age, gender = "male"] = person;
 console.log(fname, age, gender);
 console.log(person);
