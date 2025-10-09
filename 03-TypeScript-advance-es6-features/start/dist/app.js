b16d48344b9e11c2f3672d5fae7b90b70a424a3f
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-09 20:23:37 +0300

    Array Destructuring

--- 03-TypeScript-advance-es6-features/start/dist/app.js
+++ 03-TypeScript-advance-es6-features/start/dist/app.js
@@ -1,6 +1,7 @@
 "use strict";
-// Array Destructuring
-const person = ["John", "Smith", 28];
+//  Destructuring is a feature that was introduced in Es6(Ecmascript2015) that allows us to extract values/elements from arrays or properties from objects into distinct variables
+// Array destructuring:Is a modern (Es6) feature that allows you to unpack elements from an array into individual variables in a concise and readable way.
+const person = ["John", "Smith", 28]; //const person: (string | number)[]
 const [fname, lmane, age, gender = "male"] = person;
 console.log(fname, age, gender);
 console.log(person);
--- 03-TypeScript-advance-es6-features/start/src/app.ts
+++ 03-TypeScript-advance-es6-features/start/src/app.ts
@@ -1,5 +1,8 @@
-// Array Destructuring
-const person = ["John", "Smith", 28];
+//  Destructuring is a feature that was introduced in Es6(Ecmascript2015) that allows us to extract values/elements from arrays or properties from objects into distinct variables
+
+// Array destructuring:Is a modern (Es6) feature that allows you to unpack elements from an array into individual variables in a concise and readable way.
+
+const person: (string | number)[] = ["John", "Smith", 28]; //const person: (string | number)[]
 
 const [fname, lmane, age, gender = "male"] = person;
 console.log(fname, age, gender);
