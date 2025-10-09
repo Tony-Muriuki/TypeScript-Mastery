3876179a5a2c25fb182bc06e60621a16b3108cf9
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-09 20:43:36 +0300

    Array destructuring

--- 03-TypeScript-advance-es6-features/start/dist/app.js
+++ 03-TypeScript-advance-es6-features/start/dist/app.js
@@ -1,7 +1,21 @@
 "use strict";
 //  Destructuring is a feature that was introduced in Es6(Ecmascript2015) that allows us to extract values/elements from arrays or properties from objects into distinct variables
 // Array destructuring:Is a modern (Es6) feature that allows you to unpack elements from an array into individual variables in a concise and readable way.
+/*Syntax For array destructuring Syntax const [arr1,arr2]= array */
 const person = ["John", "Smith", 28]; //const person: (string | number)[]
 const [fname, lmane, age, gender = "male"] = person;
 console.log(fname, age, gender);
 console.log(person);
+//Skipping Elements: You can skip by leaving blank spaces between commas
+const numarr = [10, 9, 8, 7, 6, 5];
+const [el1, , el3, el4, el5, el6] = numarr;
+console.log(el3, "Should be 8");
+//Nested Array Destructuring
+const nested = [2, 4, [5, 6]];
+const [num1, , multiarray] = nested;
+const [num3, num4] = multiarray;
+console.log(multiarray);
+console.log(num1, num3, num4);
+// Destructuring is a feature that was introduced in Es6(Ecmascript 2015)that allows you to extract values/elements from an array or properties from an object into distinct variables.
+// Object Destructurring : Obj Syntax    const { prop1, prop2 } = object;<---Use Curlybraces
+//
--- 03-TypeScript-advance-es6-features/start/src/app.ts
+++ 03-TypeScript-advance-es6-features/start/src/app.ts
@@ -2,8 +2,29 @@
 
 // Array destructuring:Is a modern (Es6) feature that allows you to unpack elements from an array into individual variables in a concise and readable way.
 
+/*Syntax For array destructuring Syntax const [arr1,arr2]= array */
+
 const person: (string | number)[] = ["John", "Smith", 28]; //const person: (string | number)[]
 
 const [fname, lmane, age, gender = "male"] = person;
 console.log(fname, age, gender);
 console.log(person);
+
+//Skipping Elements: You can skip by leaving blank spaces between commas
+const numarr: number[] = [10, 9, 8, 7, 6, 5];
+
+const [el1, , el3, el4, el5, el6] = numarr;
+console.log(el3, "Should be 8");
+
+//Nested Array Destructuring
+const nested = [2, 4, [5, 6]];
+
+const [num1, , multiarray] = nested;
+const [num3, num4] = multiarray as number[];
+console.log(multiarray);
+console.log(num1, num3, num4);
+// Destructuring is a feature that was introduced in Es6(Ecmascript 2015)that allows you to extract values/elements from an array or properties from an object into distinct variables.
+
+// Object Destructurring : Obj Syntax    const { prop1, prop2 } = object;<---Use Curlybraces
+
+//
