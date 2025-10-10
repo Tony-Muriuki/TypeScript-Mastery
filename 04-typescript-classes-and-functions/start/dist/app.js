3f5688c00f1affed6d25b2dcc36e7a7f545111fa
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-10 12:48:31 +0300

    OOP TS

--- 04-typescript-classes-and-functions/start/dist/app.js
+++ 04-typescript-classes-and-functions/start/dist/app.js
@@ -1,5 +1,5 @@
+"use strict";
 //Classes and Functions
-
 // Object oriented programming is a programming paradigm(style of writing and organizing code) based on the concepts of objects.
 /*
 1:Objects can contain data which we call Properties and code which we call Methods.
@@ -7,56 +7,41 @@
 // A class is a blueprint for creating Objects(instances)......Defines Data(properties)+behaviour(methods)
 
 */
-
 // Lecture 37 :Creating A Class and Using A Class
-
-class User {
-  firstname: string;
-  lastname: string;
-  age: number;
-  gender: string;
-
-  constructor(fn: string, ln: string, a: number, g: string) {
-    this.firstname = fn;
-    this.lastname = ln;
-    this.age = a;
-    this.gender = g;
-  }
-
-  greetuser(salutation: string) {
-    const msg = `Hello ${salutation} .  ${this.firstname} ${this.lastname}`;
-    console.log(msg);
-  }
-}
-
-const user1 = new User("john", "smith", 28, "male");
-const user2 = new User("merry", "jane", 32, "female");
-const user3 = new User("mark", "vought", 29, "male");
-
+var User = /** @class */ (function () {
+    function User(fn, ln, a, g) {
+        this.firstname = fn;
+        this.lastname = ln;
+        this.age = a;
+        this.gender = g;
+    }
+    User.prototype.greetuser = function (salutation) {
+        var msg = "Hello ".concat(salutation, " .  ").concat(this.firstname, " ").concat(this.lastname);
+        console.log(msg);
+    };
+    return User;
+}());
+var user1 = new User("john", "smith", 28, "male");
+var user2 = new User("merry", "jane", 32, "female");
+var user3 = new User("mark", "vought", 29, "male");
 // Invoking Greet User
 user1.greetuser("Mrs");
-
 // The Prototype Chain
-
 /*1: user1 (the instance)
 → has its own properties: name, age, gender.
 Prototype link:*/
 Object.getPrototypeOf(user1) === User.prototype; // ✅ true
-
 /*User.prototype (the prototype object of the class)
 → Contains methods shared by all User instances (like User.prototype.constructor).
 Prototype link: */
 Object.getPrototypeOf(User.prototype) === Object.prototype; // ✅ true
-
 /*Object.prototype (the base of all objects)
 → Provides built-in methods like .toString(), .hasOwnProperty(), etc.
 Prototype link: */
 Object.getPrototypeOf(Object.prototype) === null; // ✅ true
-
 /*→ Marks the end of the chain — no further prototype above this. */
-
 /*
-FULLL VISUAL CHAIN 
+FULLL VISUAL CHAIN
 user1
   ↓ [[Prototype]]
 User.prototype
