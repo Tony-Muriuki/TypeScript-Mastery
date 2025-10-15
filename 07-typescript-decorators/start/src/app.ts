0183b42d567c83e08bc133c413cb60e5e8667c2e
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-15 12:29:31 +0300

    Decorators TS

--- 07-typescript-decorators/start/src/app.ts
+++ 07-typescript-decorators/start/src/app.ts
@@ -1,3 +1,7 @@
 /***********************************************
  * *****UNDERSTANDING DECORATORS****************
  ***********************************************/
+
+// Decorators are powerful syntactic feature in Typescript that allows you to add functionality or modify the behaviour of classes , methods and  properties at runtime.
+
+// Decorators are essentially functions that are applied using the @ symbol before the declarationn you want to decorate
